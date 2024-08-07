73cff9fb8144a1d759215f05d8cee7899bc9f92c
Pawan Chaukiyal <pawanchaukiyal@gmail.com>
2024-08-07 15:47:04 +0530

    card--pc

---
+++ src/section/common/Common1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Common1 = () => {
+  return (
+    <div>Common1</div>
+  )
+}
+
+export default Common1
---
+++ src/section/yoga/YogaData.js
@@ -0,0 +1,18 @@
+export const yogdata = [
+    {
+      imglink: 'https://images.unsplash.com/photo-1722232581651-d87e4099561b',
+      heading: 'Beginner',
+      text: 'click it...',
+    },
+    {
+      imglink: 'https://images.unsplash.com/photo-1722232581651-d87e4099561b',
+      heading: 'Intermediate',
+      text: 'click it...',
+    },
+    {
+      imglink: 'https://images.unsplash.com/photo-1722232581651-d87e4099561b',
+      heading: 'Advanced',
+      text: 'click it...',
+    },
+  ];
+  
