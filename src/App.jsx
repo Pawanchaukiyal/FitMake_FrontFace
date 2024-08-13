08d0f95edaa5a03747421e894130b7cb13742ab4
deepu123455432 <166910981+deepu123455432@users.noreply.github.com>
2024-08-13 13:28:57 +0530

    dp=fix all cards

--- src/App.jsx
+++ src/App.jsx
@@ -18,42 +18,39 @@ import CommonUnifiedAot from "./section/common/CommonUnifiedAot";
 import CommonPick from "./section/common/CommonPick";
 import Equip from "./section/common/Equip";
 
-
-
 const App = () => {
   return (
-  <div className=".back__bg">
-  <ScrollTop />
-  <Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/explore" element={<Explore />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/signup" element={<Signup />} />
-  <Route path="/logout" element={<Logout />} />
-  {/* done */}
-  <Route path="/yogalevel/:yogalevel" element={<CommonUnifiedLevel/>} />
-  {/* done */}
-  <Route path="/exerciselevel/:exerciselevel" element={<CommonUnifiedLevel />} />
-  {/* done */}
-  <Route path="/exerciseaot/:exerciseaot" element={<CommonUnifiedAot />} />
-
-  <Route path="/yogaaot/:yogaaot" element={<CommonUnifiedAot />} />
-
-{/* done */}
-  <Route path="/combined/:value" element={<CommonPick />} /> {/* For combined data */}
-  <Route path="/yogalevel/:value" element={<CommonPick />} />
-
-
-
-  <Route path="/aot/:value" element={<Commonaot />} />
-  <Route path="/aot/:value" element={<Commonaot />} />
-
-
-  <Route path="/category/:value" element={<Equip />} />
-
-  <Route path="/exercise/exerciseaot/:value" element={<Common4 />} />
-  <Route path="/start" element={<Start />} />
-</Routes>
+    <div className=".back__bg">
+      <ScrollTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/logout" element={<Logout />} />
+        {/* done */}
+        <Route path="/yogalevel/:yogalevel" element={<CommonUnifiedLevel />} />
+        {/* done */}
+        <Route
+          path="/exerciselevel/:exerciselevel"
+          element={<CommonUnifiedLevel />}
+        />
+        {/* done */}
+        <Route
+          path="/exerciseaot/:exerciseaot"
+          element={<CommonUnifiedAot />}
+        />
+        <Route path="/yogaaot/:yogaaot" element={<CommonUnifiedAot />} />
+        {/* done */}
+        <Route path="/combined/:value" element={<CommonPick />} />{" "}
+        {/* For combined data */}
+        <Route path="/yogalevel/:value" element={<CommonPick />} />
+        <Route path="/aot/:value" element={<Commonaot />} />
+        <Route path="/aot/:value" element={<Commonaot />} />
+        <Route path="/category/:value" element={<Equip />} />
+        <Route path="/exercise/exerciseaot/:value" element={<Common4 />} />
+        <Route path="/start" element={<Start />} />
+      </Routes>
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import Carousel from '../components/carousel/Carousel'
-import Layout from '../components/layout/Layout'
-import Yoga from '../section/HomeSection/yoga/Yoga'
-import Exercise from '../section/HomeSection/exercise/Exercise'
-import BodyFocus from '../section/HomeSection/bodyfocussection/BodyFocus'
+import React from "react";
+import Carousel from "../components/carousel/Carousel";
+import Layout from "../components/layout/Layout";
+import Yoga from "../section/HomeSection/yoga/Yoga";
+import Exercise from "../section/HomeSection/exercise/Exercise";
+import BodyFocus from "../section/HomeSection/bodyfocussection/BodyFocus";
 
-
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-
+  const navigate = useNavigate();
   return (
     <Layout>
-    <Carousel/>
-     <Yoga/>
-     <Exercise/>
-     <BodyFocus/>
-     <span className='block rounded-xl m-4 bg-yellow-400 text-center p-4 font-serif mb-4 font-bold text-4xl '>Click Here To Discover More</span>
+      <Carousel />
+      <Yoga />
+      <Exercise />
+      <BodyFocus />
+      <span
+        className="block rounded-xl m-4 bg-yellow-400 text-center p-4 font-serif mb-4 font-bold text-4xl cursor-pointer "
+        onClick={() => navigate("/explore")}
+      >
+        Click Here To Discover More
+      </span>
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
